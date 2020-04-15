992b8cb8064548d48ebaede04231019e1a18f75a
wugaoFQA <1162423676@qq.com>
2020-04-15 18:26:52 +0800

    first commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call(UsersSeeder::class);
+        $this->call(UserAddressesSeeder::class);
+        // 放在 ProductsSeeder 之前
+        $this->call(CategoriesSeeder::class);
+        $this->call(ProductsSeeder::class);
+        $this->call(CouponCodesSeeder::class);
+        $this->call(OrdersSeeder::class);
+    }
+}
